05f6467519975bc03690fc0a45e5fbef99417feb
LevonKhazhoyan <levon_hazhoyan@mail.ru>
2023-03-31 23:38:59 +0300

    pointfree done

---
+++ PointFree/Functions.fs
@@ -0,0 +1,11 @@
+﻿module Functions
+
+let myFunction x l = List.map (fun y -> y * x) l
+
+let myFunction2 x = List.map (fun y -> y * x)
+
+let myFunction3 x = List.map ((*) x)
+
+let myFunction4 x = List.map << (*) << x
+
+let pointFree = List.map << (*)
---
+++ PointFree/PointFree/Library.fs
@@ -0,0 +1,5 @@
+﻿namespace PointFree
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
