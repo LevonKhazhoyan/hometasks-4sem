bf0d4ed08054022db7b5a6793fbbed3416fc9534
LevonKhazhoyan <levon_hazhoyan@mail.ru>
2023-04-21 00:51:47 +0300

    string calculation builder is done

---
+++ StringCalc/StringCalc/StringCalcBuilder.fs
@@ -0,0 +1,12 @@
+﻿module StringCalc
+
+open System
+
+/// Workflow for counting with numbers given as a string
+type StringCalculatorBuilder () =
+    member this.Bind(x: string, func) = 
+        try 
+            x |> int |> func
+        with :? FormatException
+            -> None
+    member this.Return(x) = x |> Some
