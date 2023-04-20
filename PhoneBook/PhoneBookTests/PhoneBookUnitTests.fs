1a10032b25b3185e810e5de05c7816341de6504f
LevonKhazhoyan <levon_hazhoyan@mail.ru>
2023-04-21 00:32:08 +0300

    changed path

--- PhoneBook/PhoneBookTests/PhoneBookUnitTests.fs
+++ PhoneBook/PhoneBookTests/PhoneBookUnitTests.fs
@@ -7,11 +7,11 @@ open PhoneBookFunctions
 [<Test>]
 let ``Test for readFromFile`` () =
     let book = [{ Name = "Name"; Number = "85"}; {Name = "Me"; Number = "87"}; {Name =  "Lizz"; Number = "15"}]
-    readFromFile "..\\..\\..\\testBook.txt" |> should equal book
+    readFromFile "..//..//..//testBook.txt" |> should equal book
 
 [<Test>]
 let ``Test for printToFile`` () =
-    let path = "..\\..\\..\\test.txt"
+    let path = "..//..//..//test.txt"
     let book = [{ Name = "Name"; Number = "85"}; {Name = "Me"; Number = "87"}; {Name =  "Lizz"; Number = "15"}]
     printToFile book path
     readFromFile path |> should equal book
