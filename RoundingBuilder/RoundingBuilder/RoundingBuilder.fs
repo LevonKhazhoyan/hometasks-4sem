d6fc966848c63bc5518861c8df1877a0cfd9658f
LevonKhazhoyan <levon_hazhoyan@mail.ru>
2023-04-21 01:06:18 +0300

    rounding builder is done

---
+++ RoundingBuilder/RoundingBuilder/RoundingBuilder.fs
@@ -0,0 +1,8 @@
+﻿module RoundingBuilder
+
+open System
+
+/// Workflow for counting with given accuracy 
+type RounderBuilder (digits: int) =
+    member this.Bind(x, func) = func x
+    member this.Return(x: float) = Math.Round(x, digits)
