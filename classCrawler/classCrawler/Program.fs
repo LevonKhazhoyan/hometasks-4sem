a8ee2e5136287dc6f8b625d351c800701df8b217
LevonKhazhoyan <levon_hazhoyan@mail.ru>
2023-05-20 18:15:29 +0300

    crawler is done

---
+++ classCrawler/classCrawler/Program.fs
@@ -0,0 +1,52 @@
+﻿open System
+open System.Collections.Generic
+open System.IO
+open System.Text.RegularExpressions
+open System.Threading.Tasks
+
+let searchClassInstancesAsync directoryPath =
+    let csharpFilePattern = "*.cs"
+    let classNamePattern = @"(?:\bnew\s+)(?<ClassName>[A-Za-z_][A-Za-z0-9_]*)(?:\s*\()"
+
+    let getFilesWithExtensionAsync path extension =
+        Task.Run(fun () -> Directory.GetFiles(path, extension, SearchOption.AllDirectories))
+
+    let extractClassInstancesAsync filePath classNamePattern (output: List<string>) =
+        async {
+            let! fileLines = File.ReadAllLinesAsync(filePath) |> Async.AwaitTask
+            fileLines
+            |> Seq.mapi (fun index line ->
+                Regex.Matches(line, classNamePattern)
+                |> Seq.cast<Match>
+                |> Seq.map (fun matchObj -> matchObj.Groups.["ClassName"].Value, index + 1, filePath)
+            ) |> Seq.concat
+            |> Seq.iter (fun (className, lineIndex, file) ->
+                let result = $"In file {file} instance of class {className} is created at line {lineIndex}"
+                output.Add(result))
+        }
+
+    let searchAsync () =
+        async {
+            let files = getFilesWithExtensionAsync directoryPath csharpFilePattern |> Async.AwaitTask |> Async.RunSynchronously
+            let output = List<string>()
+            let tasks =
+                files
+                |> Seq.map (fun file -> extractClassInstancesAsync file classNamePattern output)
+
+            do! tasks |> Async.Parallel |> Async.Ignore
+
+            return output
+        }
+
+    async {
+        let! results = searchAsync()
+        for item in results do
+            printfn $"%s{item}"
+    }
+[<EntryPoint>]
+let main _ =
+    let directoryName = Console.ReadLine()
+    async {
+        do! searchClassInstancesAsync directoryName
+    } |> Async.RunSynchronously
+    0
