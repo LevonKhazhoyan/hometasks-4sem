8f3a7cbc8b6661ac9d3499d066b1c4a6a70cebf2
LevonKhazhoyan <levon_hazhoyan@mail.ru>
2023-05-21 17:16:26 +0300

    fibonacci is done

---
+++ Fibonacci/Fibonacci/Fibonacci.fs
@@ -0,0 +1,11 @@
+﻿module FibonacciFunc
+
+/// Fibonacci list with specified lenght
+let fibonacci n =
+    if n < 0 then failwith "Expected non-negative n"
+    let rec loop acc1 acc2 n =
+        match n with
+        | 0 -> []
+        | n -> acc1 :: loop acc2 (acc1 + acc2) (n - 1)
+    
+    loop 0 1 (n + 1)
