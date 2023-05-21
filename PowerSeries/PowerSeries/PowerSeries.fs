f55da38725813c163f0aa93a177f2200e3378bd7
LevonKhazhoyan <levon_hazhoyan@mail.ru>
2023-05-21 22:17:07 +0300

    power series is done

---
+++ PowerSeries/PowerSeries/PowerSeries.fs
@@ -0,0 +1,12 @@
+﻿module PowerSeriesFunc
+
+let listOfPowerSeries n m =
+    if n < 0 || m < 0 then failwith "Expected positive n and m"
+    let rec loop acc value length =
+        if length = m + 1 then
+            acc
+        else
+            loop (value :: acc) (value / 2) (length + 1)
+
+    loop [] (pown 2 (n + m)) 0
+    
